dfd20223dd4c864efdb9b0f6240a268783c1aea6
eastpiger <lvwt@shanghaitech.edu.cn>
2015-05-12 14:53:29 +0800

    a

--- src/core/debug.c
+++ src/core/debug.c
@@ -3,6 +3,30 @@
 #include "../include/prep.h"
 #include "../include/debug.h"
 
+int ADIInit(RGBPoint *mat,char matyuv[480][640][2]){
+	int i,j;
+	double y=1;
+	double u=1;
+	double v=1;
+	char handle=1;
+	for (j=0;j<MAT_WIDTH;j++){
+		for (i=0;i<MAT_HEIGHT;i++){
+			y=matyuv[i][j][0];
+			if (handle){
+				u=matyuv[i][j][1];
+				handle=0;
+			}else{
+				v=matyuv[i][j][1];
+				handle=1;
+			}
+			(*(mat+i*(MAT_WIDTH)+j)).r=y+1.4075*v;
+			(*(mat+i*(MAT_WIDTH)+j)).g=y-0.455*u-0.7169*v;
+			(*(mat+i*(MAT_WIDTH)+j)).b=y+1.779*u;
+		}
+	}
+	return 0;
+}
+
 int Init(RGBPoint *mat){
 	FILE *in;
 	in=fopen(DEFAULT_INPUT,"r");
--- src/include/debug.h
+++ src/include/debug.h
@@ -1,6 +1,7 @@
 //debug
 
 int Init(RGBPoint *mat);
+int ADIInit(RGBPoint *mat,char matyuv[480][640][2]);
 int PrintRGBMat(RGBPoint *mat,char name[]);
 int PrintAMat(APoint *mat,char name[]);
 int PrintMat(Point *mat,char name[]);
